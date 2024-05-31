3691bfe9c823db41252e1c86cc1a24a4eeb825de
Kaio Victor <102392681+MsTzz@users.noreply.github.com>
2024-05-31 04:10:41 -0300

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import Routes from './src/routes';
 import { NavigationContainer} from '@react-navigation/native'
 
 export default function App(){
+    
     return(
         <NavigationContainer>
             <Routes />
